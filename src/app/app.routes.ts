6a0993f18ebbe9e6c82883313993c1fd81d34e2b
tiagomac29 <162623317+tiagomac29@users.noreply.github.com>
2025-05-14 08:57:15 -0300

    Delete src directory

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { FormularioComponent } from './formulario/formulario.component';
-import { HomeComponent } from './home/home.component';
-
-export const routes: Routes = [ // Adicione o "export" para ser importado em outros arquivos
-    { path: '', component: HomeComponent },
-    { path: 'formulario', component: FormularioComponent }
-  ];
