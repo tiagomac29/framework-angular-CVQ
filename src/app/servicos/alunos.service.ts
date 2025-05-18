692a231e40073576743407cd01862c59079b2bb9
tiagomac29 <162623317+tiagomac29@users.noreply.github.com>
2025-05-18 16:55:45 -0300

    Update alunos.service.ts

--- src/app/servicos/alunos.service.ts
+++ src/app/servicos/alunos.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Aluno } from '../modelo/aluno.modelo';
+
+interface ApiRetorno{
+  erro:boolean;
+  mensagem: string;
+  dados: Aluno[]
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlunosService {
 
-  constructor() { }
+  private readonly API_URL : string = "http://localhost/apiColetivo/api/index.php?url=api/ajuda"
+
+  constructor(private http : HttpClient) { }
+
+  criar(aluno: Aluno): Observable <ApiRetorno>{
+    return this.http.post<ApiRetorno>(this.API_URL, aluno)
+  }
+
+  listarTodos(): Observable <ApiRetorno> {
+ return this.http.get<ApiRetorno>(this.API_URL);
 }
+
+
+}
+
